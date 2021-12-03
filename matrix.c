12a4b75f2895b66d0d149833d3b34c67eb7a6d22
Dianamoon18 <72127610+Diamoon18@users.noreply.github.com>
2021-12-03 17:49:30 +0100

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "mpi.h"
+
+int main (int argc, char** argv) {
+
+    int nr_proc, size;
+    MPI_Datatype diag;
+
+    /* MPI Initialization */
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &nr_proc);
+
+    /* Matrix initialization */
+    int matrix[size][size];
+
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++){
+            if (i==j) 
+                matrix[i][j] = nr_proc;
+ 	        else 
+                matrix[i][j] = 0;
+        }
+	}
+	
+    /* Print rank 0 matrix (should be filled with 0s) */
+    if (nr_proc == 0) {
+       printf("Process nr-0 -> matrix before communication:\n");
+       for (int i = 0; i < size; i++) {
+           for (int j = 0; j < size; j++){
+               printf("%d ", matrix[i][j]);
+           }
+           printf("\n");
+       }
+       printf("\n"); 
+    }
+
+    /* Diagonal datatype vector creation */
+    MPI_Type_vector(size, 1, size+1, MPI_INT,&diag);
+    MPI_Type_commit(&diag);
+
+    /* Communication: rank 0 gathers all the diagonals from the other ranks and stores them in the row corresponding to the
+     * sending rank. Note that 1 "diag" type is sent and size MPI_INT types are received, so that the values can be stored
+     * contiguously in the receiving matrix */
+    MPI_Gather(matrix, 1, diag, matrix, size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    /* Print rank 0 matrix after communication (each element should be its row number) */
+    if (nr_proc == 0) {
+       printf("Process nr-0 -> matrix after communication:\n");
+       for (int i = 0; i < size; i++) {
+           for (int j = 0; j < size; j++){
+                printf("%d ", matrix[i][j]);
+           }
+           printf("\n");
+       }
+       printf("\n");
+    }
+
+    /* Remember to free the datatype! */
+    MPI_Type_free(&diag);
+    MPI_Finalize();
+    return 0;
+}
+
